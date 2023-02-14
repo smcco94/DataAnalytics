60650a362012c0ba4351a4bdc40908c438caeaf9
smcco94 <sergiomcastro94@gmail.com>
2023-02-13 22:03:53 -0300

    Aula 13/02

--- Formação_Data_Sciency/Modulo_1/Data_Sciency/dados&visualizacoes.py
+++ Formação_Data_Sciency/Modulo_1/Data_Sciency/dados&visualizacoes.py
@@ -1,2 +1,45 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Lê arquivo csv
+df = pd.read_csv("../files/ratings.csv")
+
+# printa primeiras 10 linhas
+df.head(10)
+
+#Renomeia coluna
+df.columns = ["UsuarioID","FilmeID","Nota","Momento"]
+
+#verifica tipo
+type(df)
+
+#printa coluna
+df.Nota
+df['Nota']
+
+#contagem de valores
+df['Nota'].value_counts()
+df.Nota.value_counts()
+
+#média das notas
+df['Nota'].mean()
+df.Nota.mean()
+
+#Gráfico info adicional (https://matplotlib.org/stable/plot_types/index.html)
+df.Nota.plot(kind='hist')
+plt.axvline(df.Nota.median(), color='k', linestyle='dashed', linewidth=1, label='sine')
+plt.axhline(df.Nota.value_counts().mean(), color='k', linestyle='dashed', linewidth=1)
+
+#boxplot
+df.Nota.plot(kind='box', vert=False)
+
+#mediana e media
+print("Média",df.Nota.mean())
+print("Mediana",df.Nota.median())
+
+#Conjunto de informações
+df.Nota.describe()
+
+#Boxplot
+sns.boxplot(df.Nota)
